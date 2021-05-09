2cfccd3ed9fbd5c15d084fa560ce64df11eb4404
Automated Publisher <actions@users.noreply.github.com>
2021-05-09 15:32:50 +0000

    Automated publish: Sun May  9 15:32:50 UTC 2021 7e9e18e567717ff28d8637c65b85ac0d4b33bd25

--- pages/script-bootstrap.js
+++ pages/script-bootstrap.js
@@ -1,13 +1,14 @@
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipTriggerList = [].slice.call(
+  document.querySelectorAll('[data-bs-toggle="tooltip"]'),
+)
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
 })
-
 
 var exampleEl = document.getElementById('example')
 var tooltip = new bootstrap.Tooltip(exampleEl, options)
 
 var exampleEl = document.getElementById('example')
 var tooltip = new bootstrap.Tooltip(exampleEl, {
-  boundary: document.body // or document.querySelector('#boundary')
+  boundary: document.body, // or document.querySelector('#boundary')
 })
