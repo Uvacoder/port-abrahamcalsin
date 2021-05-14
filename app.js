e3930972cc1985c3a95a6b19687393694ec31b28
Automated Publisher <actions@users.noreply.github.com>
2021-05-14 07:06:06 +0000

    Automated publish: Fri May 14 07:06:06 UTC 2021 573b3204465154b91ca2b031f7dc74a8ac27a703

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-function init(){
-    const disabled = document.getElementById('linkDisabled');
-    disabled.onclick = function(){return false};
+function init() {
+  const disabled = document.getElementById('linkDisabled')
+  disabled.onclick = function () {
+    return false
+  }
 }
