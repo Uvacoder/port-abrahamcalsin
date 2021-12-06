64857c96a6161ff66bef80f5f13447bd6a3b2775
abrahamcalsin <hello.abrahamcalsin@gmail.com>
2021-12-05 22:46:43 -0500

    feat: 🎸 dark theme added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   mode: "jit",
   purge: ["./src/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
-  darkMode: "class", // or 'media' or 'class'
+  darkMode: "class",
   theme: {
     screens: {
       sm: "640px",
