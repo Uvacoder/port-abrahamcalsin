7d359002b64abca2dc5cc5f0ba7e1bf3d60d215c
Abraham Calsin <hello.abrahamcalsin@gmail.com>
2022-03-14 11:35:50 -0500

    style: 💄 update date formatter styles

--- src/components/date-format-article/index.tsx
+++ src/components/date-format-article/index.tsx
@@ -9,8 +9,6 @@ export const DateFormatArticle = (props: DateFormatArticleProps) => {
   const { dateArticle = new Date() } = props
 
   return (
-    <span className="mr-2 font-regular text-primary-600 dark:text-inherit italic">
-      {dayjs(dateArticle).format(dateFormat.article)}
-    </span>
+    <span className="font-regular text-inherit dark:text-inherit">{dayjs(dateArticle).format(dateFormat.article)}</span>
   )
 }
