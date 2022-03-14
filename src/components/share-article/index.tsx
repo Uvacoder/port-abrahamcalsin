a15cc9dd822aeb58e27f70122fd6a722d25d4ef9
Abraham Calsin <hello.abrahamcalsin@gmail.com>
2022-03-14 11:05:28 -0500

    update the design of the share button

--- src/components/share-article/index.tsx
+++ src/components/share-article/index.tsx
@@ -9,9 +9,9 @@ export const ShareArticle = (props: ShareArticleProps) => {
   const { articleLink, articleTitle } = props
 
   return (
-    <div className="inline-flex items-center gap-x-1">
+    <div className="w-full inline-flex items-center justify-center gap-x-1 sm:sticky mt-2.5 sm:bottom-10">
       <a
-        className="flex items-center gap-x-0.7 not-italic text-sm mt-1 sm:mt-0 transition-opacity link-underline"
+        className="flex items-center gap-x-0.7 not-italic text-sm bg-[#1EA1F1] hover:bg-[#57B3EB] shadow-lg shadow-primary/20 text-primary-50 font-semiBold px-2.2 py-1 rounded-full"
         target="_blank"
         rel="nofollow noopener noreferrer"
         href={`https://twitter.com/share?url=https://abrahamcalsin.com/blog/${articleLink}&text=" ${articleTitle} " por @abraham_calsin`}
