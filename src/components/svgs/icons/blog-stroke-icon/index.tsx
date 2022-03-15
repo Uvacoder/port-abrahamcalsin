67a2c879c91b9582a1ef23a7aa991c73c8cb73c4
Abraham Calsin <hello.abrahamcalsin@gmail.com>
2022-03-14 22:32:31 -0500

    perf: ⚡️ modify the code of the icon component

--- src/components/svgs/icons/blog-stroke-icon/index.tsx
+++ src/components/svgs/icons/blog-stroke-icon/index.tsx
@@ -5,8 +5,6 @@ export const BlogStrokeIcon = () => {
     <svg className="block w-2.2" viewBox="0 0 15 16" xmlns="http://www.w3.org/2000/svg">
       <path d="M9.11279 14.2564H14.427" stroke="currentColor" strokeWidth="1.3842" />
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
         d="M11.3815 2.04905V2.04905C10.2615 1.20905 8.6732 1.43572 7.8332 2.55489C7.8332 2.55489 3.6557 8.11989 2.20653 10.0507C0.757367 11.9824 2.1282 14.3757 2.1282 14.3757C2.1282 14.3757 4.83153 14.9974 6.25987 13.0932C7.68903 11.1899 11.8865 5.59739 11.8865 5.59739C12.7265 4.47822 12.5007 2.88905 11.3815 2.04905Z"
         stroke="currentColor"
         strokeWidth="1.3842"
