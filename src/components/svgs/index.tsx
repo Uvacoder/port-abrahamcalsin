a18bedae78517210ba53a79b3cf788b08a165e1c
Abraham Calsin <hello.abrahamcalsin@gmail.com>
2022-03-18 18:48:04 -0500

    add icon components to a single export address

---
+++ src/components/svgs/index.tsx
@@ -0,0 +1,10 @@
+export * from './logo-abrahamcalsin'
+
+export * from './linkedin-social-network'
+export * from './github-social-network'
+
+export * from './reactjs-technology'
+export * from './nextjs-technology'
+export * from './typescript-technology'
+export * from './tailwindcss-technology'
+export * from './vercel-technology'
