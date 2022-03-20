54ce313f96c3544c0ebcf00b311c1fc01d3e5b4c
Abraham Calsin <hello.abrahamcalsin@gmail.com>
2022-03-20 17:41:46 -0500

    refactor: 💡 improve the code of the dark theme button

---
+++ src/components/dark-theme-button/dark-theme-button.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react'
+
+import useDarkTheme from '~/hook/use-dark-theme'
+
+import { MoonFillIcon } from './components/moon-fill-icon'
+import { SunStrokeIcon } from './components/sun-stroke-icon'
+
+export const DarkThemeButton = () => {
+  const { onToggle } = useDarkTheme()
+
+  const styles: React.CSSProperties = {
+    position: 'absolute',
+    transition: 'transform 300ms ease-in-out',
+  }
+
+  return (
+    <button
+      onClick={onToggle}
+      className="relative flex flex-wrap wrap items-center w-3.7 h-3.7 p-0.7 dark:bg-secondary/10 border-px border-primary dark:border-secondary text-primary dark:text-secondary rounded overflow-hidden select-none transition-all"
+    >
+      <div className="translate-y-0 dark:translate-y-6" style={styles}>
+        <SunStrokeIcon />
+      </div>
+      <div className="translate-y-6 dark:translate-y-0" style={styles}>
+        <MoonFillIcon />
+      </div>
+    </button>
+  )
+}
