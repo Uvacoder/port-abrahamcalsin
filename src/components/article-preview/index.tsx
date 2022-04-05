1bdd24928f73b9f12c4d81ad52f725efda2a3572
abrahamcalsin <hello.abrahamcalsin@gmail.com>
2022-04-04 19:39:31 -0500

    refactor: 💡 modify the style of article previews

--- src/components/article-preview/index.tsx
+++ src/components/article-preview/index.tsx
@@ -6,27 +6,28 @@ import { CoffeeStrokeIcon } from '~/components/svgs/icons'
 interface ArticlePreviewProps {
   title: string
   date: string
-  description: string
   slug: string
   readingTime: string
 }
 
 export const ArticlePreview = (props: ArticlePreviewProps) => {
-  const { title, date, description, slug, readingTime } = props
+  const { title, date, slug, readingTime } = props
 
   return (
-    <div className="mb-5 bg-primary-100/50 dark:bg-white/10 hover:opacity-80 transition-opacity rounded-md border-px border-primary-300 dark:border-primary-500">
+    <div className="border-y-px border-primary-500/20 py-2 hover:opacity-80">
       <Link href={`/blog/${slug}`} passHref>
-        <a className="p-2.5 block">
-          <h3 className="text-primary-700 dark:text-primary-400 mb-1 line-clamp-1 font-bold">{title}</h3>
-          <p className="line-clamp-2 mb-1 text-base text-primary-500">{description}</p>
-          <p className="flex items-center mb-0 font-regular text-sm text-primary-500">
-            <DateFormatArticle dateArticle={date} />
-            <span className="mx-1.2">|</span>
-            <p className="flex text-sm font-regular items-center gap-1 m-0">
-              <CoffeeStrokeIcon className="w-2" />
-              <span>{readingTime}</span>
+        <a className="flex items-center justify-between gap-x-2">
+          <div className="flex sm:items-center w-full flex-col sm:flex-row">
+            <p className="sm:w-[12rem] m-0 text-sm">
+              <DateFormatArticle dateArticle={date} />
             </p>
+            <h3 className="w-full text-lg text-primary-700 dark:text-primary-400 line-clamp-1 font-bold m-0 underline sm:no-underline hover:underline">
+              {title}
+            </h3>
+          </div>
+          <p className="w-[8rem] hidden sm:flex text-sm font-regular items-center gap-1 m-0">
+            <CoffeeStrokeIcon className="w-2" />
+            <span>{readingTime}</span>
           </p>
         </a>
       </Link>
