a80c1d487785b478b19d50948f9fa389b064a055
abrahamcalsin <hello.abrahamcalsin@gmail.com>
2022-04-29 00:53:07 -0500

    refactor: 💡 modify flat contact links by pills

--- pages/about-me/index.tsx
+++ pages/about-me/index.tsx
@@ -4,34 +4,13 @@ import { MainLayout } from '~/layouts/main.layout'
 import { Meta } from '~/components/meta'
 import { TitleSection } from '~/components/title-section'
 import { PillTechnologies } from '~/components/pill-technologies'
+import { PillContactLink } from '~/components/pill-contact-link'
 import { OctocatStrokeIcon, LinkedinStrokeIcon, TwitterStrokeIcon, EmailStrokeIcon } from '~/components/svgs/icons'
-
-interface ContactLinks {
-  svgIcon: React.ReactElement
-  nameIcon: string
-  href: string
-  username: string
-}
-
-const ContactLinks = (props: ContactLinks) => {
-  const { svgIcon, nameIcon, href, username } = props
-
-  return (
-    <div className="flex items-center mb-0.7 sm:mb-1.2">
-      <div className="w-2.5 mr-0.7">{svgIcon}</div>
-      <p className="mb-0 mr-1 font-bold leading-none">{nameIcon}</p>
-      <a href={href} target="_blank" className="font-medium link-underline">
-        {username}
-      </a>
-    </div>
-  )
-}
 
 const AboutMe = () => {
   return (
     <MainLayout>
       <Meta title="Sobre Mi" />
-
       <div className="mt-0 sm:mt-11.2 sm:mb-3.7 text-primary-600 dark:text-inherit">
         <TitleSection
           title="Sobre Mi"
@@ -40,12 +19,10 @@ const AboutMe = () => {
           linkText="Para más detalles, consulta mi"
           linkTextBold="CV"
         />
-
         <p>
           Hey, hola! Soy Abraham Calsin una persona autodidacta, me apasiona la programación, actualmente me desempeño
           como desarrollador frontend, creo proyectos de código abierto.
         </p>
-
         <p>
           A lo largo de mi aventura en el mundo del desarrollo web, me involucre en el desarrollo de varios proyectos,
           proponiéndome retos, los cuales me enseñaron a corregir mis propios errores al desarrollar un Sitio Web.
@@ -54,44 +31,37 @@ const AboutMe = () => {
             <a className="font-bold link-underline ml-0.7">Proyectos.</a>
           </Link>
         </p>
-
         <h3>Habilidades:</h3>
-
         <p>Estas son algunas de mis tecnologías favoritas.</p>
-
         <PillTechnologies />
-
         <h3 className="mt-2.5">Contacto:</h3>
-
         <p>Siempre puedes contactarme por los siguientes medios.</p>
-
-        <ContactLinks
-          svgIcon={<EmailStrokeIcon />}
-          nameIcon="Email:"
-          href="mailto:hello.abrahamcalsin@gmail.com"
-          username="@abrahamcalsin"
-        />
-
-        <ContactLinks
-          svgIcon={<LinkedinStrokeIcon />}
-          nameIcon="LinkedIn:"
-          href="https://www.linkedin.com/in/abrahamcalsin"
-          username="@abrahamcalsin"
-        />
-
-        <ContactLinks
-          svgIcon={<TwitterStrokeIcon />}
-          nameIcon="Twitter:"
-          href="https://twitter.com/abraham_calsin"
-          username="@abraham_calsin"
-        />
-
-        <ContactLinks
-          svgIcon={<OctocatStrokeIcon />}
-          nameIcon="GitHub:"
-          href="https://github.com/abrahamcalsin"
-          username="@abrahamcalsin"
-        />
+        <div className="grid grid-cols-2 sm:grid-cols-auto-fill gap-1 sm:gap-3">
+          <PillContactLink
+            svgIcon={<EmailStrokeIcon />}
+            socialNetworkName="Email"
+            href="mailto:hello.abrahamcalsin@gmail.com"
+            username="@abrahamcalsin"
+          />
+          <PillContactLink
+            svgIcon={<LinkedinStrokeIcon />}
+            socialNetworkName="LinkedIn"
+            href="https://www.linkedin.com/in/abrahamcalsin"
+            username="@abrahamcalsin"
+          />
+          <PillContactLink
+            svgIcon={<TwitterStrokeIcon />}
+            socialNetworkName="Twitter"
+            href="https://twitter.com/abraham_calsin"
+            username="@abraham_calsin"
+          />
+          <PillContactLink
+            svgIcon={<OctocatStrokeIcon />}
+            socialNetworkName="GitHub"
+            href="https://github.com/abrahamcalsin"
+            username="@abrahamcalsin"
+          />
+        </div>
       </div>
     </MainLayout>
   )
---
+++ src/components/pill-contact-link/index.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react'
+
+import { BadgeCheckFillIcon } from '~/components/svgs/icons'
+
+interface PillContactLinkProps {
+  href: string
+  svgIcon: React.ReactElement
+  socialNetworkName: string
+  username: string
+}
+
+export function PillContactLink(props: PillContactLinkProps) {
+  const { svgIcon, socialNetworkName, href, username } = props
+
+  return (
+    <a
+      href={href}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="group flex items-center gap-1.5 sm:gap-1.7 bg-primary-50 py-1 px-1.5 sm:py-1.2 sm:px-2 border-px border-primary-300 rounded-md hover:opacity-80"
+    >
+      <div className="w-2.5 min-w-min max-w-none sm:w-3">{svgIcon}</div>
+      <div className="flex flex-col gap-px">
+        <p className="flex items-center gap-1 mb-0 leading-none">
+          <span className="font-bold text-sm sm:text-sm -mb-px">{socialNetworkName}</span>
+          <span className="w-1.7 sm:w-2 block text-secondary">
+            <BadgeCheckFillIcon />
+          </span>
+        </p>
+        <span className="underline sm:no-underline sm:group-hover:underline text-xs sm:text-sm -mt-px block line-clamp-1">
+          {username}
+        </span>
+      </div>
+    </a>
+  )
+}
